8d082845e8213bf2a27ade8356867fabfdbf9b39
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-27 19:14:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -288,6 +288,37 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
   }
  
 });
-
+const adminprefix = ".";
+const devs = ['440856489524068352','440856489524068352'];
+client.on('message', message => {
+  var argresult = message.content.split( ).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
+      } else     
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/akrame35");
+      message.channel.sendMessage(:white_check_mark:   ${argresult})
+  }
+  });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
