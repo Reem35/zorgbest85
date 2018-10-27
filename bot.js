b8bf519782ea15f1ce65366c5088a234aa700d9f
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-27 18:12:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
 });
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = ".";
 client.on('message', message => { // Leaked by [ @M3a4x ]
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
